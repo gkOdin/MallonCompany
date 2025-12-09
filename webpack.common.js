39b36a358a7bdc4e39767a01409608d3edde1291
Carlos Henrique <oi@carloshdrp.com.br>
2025-12-08 21:17:11 -0300

    Site

---
+++ webpack.common.js
@@ -0,0 +1,24 @@
+const path = require('path');
+
+module.exports = {
+  entry: {
+    app: './js/app.js',
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+    filename: './js/app.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          'css-loader',
+          'postcss-loader'
+        ],
+      },
+    ]
+  }
+};
